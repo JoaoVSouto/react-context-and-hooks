76ba985877df18eec9f098c6af49109022822784
JoaoVSouto <joaovitorss99@hotmail.com>
2019-07-23 18:27:59 -0300

    Sets up hooks app

---
+++ hooksapp/src/components/SongList.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const SongList = () => {
+  return (
+    <div className="song-list">
+      <ul>
+        <li>To Live Again</li>
+        <li>Evil Warning</li>
+      </ul>
+    </div>
+  );
+};
+
+export default SongList;
